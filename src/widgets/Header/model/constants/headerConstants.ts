4c328e30ee81b23cdfa5289757174e38d286b490
Maksim <sharkomaks@gmail.com>
2025-12-23 18:38:31 +0300

    feat(YH-1600): add learning navigation link to header constants

--- src/widgets/Header/model/constants/headerConstants.ts
+++ src/widgets/Header/model/constants/headerConstants.ts
@@ -13,6 +13,11 @@ export const HEADER_NAV_LINKS: HeaderNavLinks[] = [
 		path: '/collections',
 		title: Header.NAV_COLLECTIONS,
 	},
+	{
+		link: ROUTES.learning.page,
+		path: '/learning',
+		title: Header.NAV_LEARNING,
+	},
 ];
 
 export const AUTH_LINKS = {
