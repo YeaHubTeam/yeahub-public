335c221386b231abc0d093737f771bde75eb7dcb
Buffik <som16@mail.ru>
2026-05-07 22:40:12 +0300

    YH-1950: add getting token to client component

--- src/widgets/Header/ui/HeaderAuth/HeaderAuthMobile/HeaderAuthMobile.tsx
+++ src/widgets/Header/ui/HeaderAuth/HeaderAuthMobile/HeaderAuthMobile.tsx
@@ -15,6 +15,32 @@ import styles from './HeaderAuthMobile.module.css';
 export const HeaderAuthMobile = () => {
 	const t = useTranslations(i18Namespace.header);
 
+	React.useEffect(() => {
+		const tryRefreshToken = async () => {
+			try {
+				const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}auth/refresh`, {
+					method: 'GET',
+					credentials: 'include',
+				});
+
+				/* eslint-disable no-console */
+				console.log('[refresh-check] status:', response.status);
+				console.log('[refresh-check] ok:', response.ok);
+
+				const data = await response.json().catch(() => null);
+				console.log('[refresh-check] data:', data);
+
+				return data;
+			} catch (error) {
+				console.log('[refresh-check] error:', error);
+				/* eslint-enable no-console */
+				return null;
+			}
+		};
+
+		void tryRefreshToken();
+	}, []);
+
 	const authMenuLinks: PopoverMenuItem[] = [
 		{
 			icon: <UserPlusIcon isCurrentColor />,
