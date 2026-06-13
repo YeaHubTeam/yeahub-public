87953e6d6a7f610c8074bde46eae1c703f051952
Gnarkill33 <gnarkill33@gmail.com>
2026-06-13 22:06:40 +0300

    YH-2071: change mapping of items as per new header link structure

--- src/widgets/Header/ui/HeaderNav/HeaderNav.tsx
+++ src/widgets/Header/ui/HeaderNav/HeaderNav.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-
 import { getLocale, getTranslations } from 'next-intl/server';
 
 import { i18Namespace } from '@/shared/config';
 
-import { HEADER_NAV_LINKS } from '../../model/constants/headerConstants';
+import { HEADER_NAV_LINKS, POPOVER_COLUMNS } from '../../model/constants/headerConstants';
 import styles from './HeaderNav.module.css';
 import { HeaderNavDesktop } from './HeaderNavDesktop/HeaderNavDesktop';
 import { HeaderNavMobile } from './HeaderNavMobile/HeaderNavMobile';
@@ -13,10 +11,21 @@ export const HeaderNav = async () => {
 	const locale = await getLocale();
 	const t = await getTranslations(i18Namespace.header);
 
-	const items = HEADER_NAV_LINKS.map(({ link, path, title }) => ({
-		href: link === '/' ? `/${locale}` : `/${locale}${link}`,
-		path,
-		label: t(title, { default: title }),
+	const items = HEADER_NAV_LINKS.map((link) => ({
+		id: link.id,
+		label: link.label,
+		href: link.path,
+		path: link.path,
+		columns: POPOVER_COLUMNS.map((column) => ({
+			id: column.id,
+			title: column.title,
+			subitems: column.subitems.map((childItem) => ({
+				name: t(childItem.title, { default: childItem.title }),
+				href: childItem.link === '/' ? `/${locale}` : `/${locale}${childItem.link}`,
+				path: childItem.path,
+				icon: childItem.icon,
+			})),
+		})),
 	}));
 
 	return (
