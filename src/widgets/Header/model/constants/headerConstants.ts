db82ba690be365c9db48fcb102113fdc56f8c682
Gnarkill33 <gnarkill33@gmail.com>
2026-06-13 22:01:09 +0300

    YH-2071: change structure of header links and types as per new header design

--- src/widgets/Header/model/constants/headerConstants.ts
+++ src/widgets/Header/model/constants/headerConstants.ts
@@ -1,44 +1,76 @@
 import { Header, ROUTES } from '@/shared/config';
 import { route } from '@/shared/libs';
 
-import { HeaderNavLinks } from '../types/headerTypes';
+import { HeaderNavLinks, PopoverColumn } from '../types/headerTypes';
 
 const initialSpecialization = 'react-frontend-developer';
 
 export const HEADER_NAV_LINKS: HeaderNavLinks[] = [
+	{ id: 'interview', label: 'Собеседование', path: '/interview' },
+	{ id: 'training', label: 'Обучение', path: '/training' },
+	{ id: 'cv', label: 'Резюме', path: '/cv' },
+];
+
+export const POPOVER_COLUMNS: PopoverColumn[] = [
 	{
-		link: route(ROUTES.questions.page, initialSpecialization),
-		path: '/questions',
-		title: Header.NAV_QUESTIONS,
+		id: 'interview',
+		title: 'Собеседование',
+		subitems: [
+			{
+				link: route(ROUTES.questions.page, initialSpecialization),
+				path: '/questions',
+				title: Header.NAV_QUESTIONS,
+				icon: 'question',
+			},
+			{
+				link: '#',
+				path: '#',
+				title: Header.NAV_COMPANIES,
+				icon: 'companies',
+			},
+			{
+				link: ROUTES.tasks.page,
+				path: '/tasks',
+				title: Header.NAV_TASKS,
+				icon: 'collection',
+			},
+		],
 	},
 	{
-		link: route(ROUTES.collections.page, initialSpecialization),
-		path: '/collections',
-		title: Header.NAV_COLLECTIONS,
+		id: 'training',
+		title: 'Обучение',
+		subitems: [
+			{
+				link: ROUTES.quiz.new.page,
+				path: '/quiz',
+				title: Header.NAV_TRAINER,
+				icon: 'quiz',
+			},
+			{
+				link: route(ROUTES.resources.page, initialSpecialization),
+				path: '/resources',
+				title: Header.NAV_RESOURCES,
+				icon: 'list',
+			},
+			{
+				link: ROUTES.learning.page,
+				path: '/learning',
+				title: Header.NAV_LEARNING,
+				icon: 'user',
+			},
+		],
 	},
 	{
-		link: ROUTES.quiz.new.page,
-		path: '/quiz',
-		title: Header.NAV_TRAINER,
-	},
-	{
-		link: route(ROUTES.resources.page, initialSpecialization),
-		path: '/resources',
-		title: Header.NAV_RESOURCES,
-	},
-	{
-		link: ROUTES.learning.page,
-		path: '/learning',
-		title: Header.NAV_LEARNING,
-	},
-	{
-		link: ROUTES.hhAnalytics.page,
-		path: '/hh-analytics',
-		title: Header.NAV_HH_ANALYTICS,
-	},
-	{
-		link: ROUTES.tasks.page,
-		path: '/tasks',
-		title: Header.NAV_TASKS,
+		id: 'cv',
+		title: 'Резюме',
+		subitems: [
+			{
+				link: ROUTES.hhAnalytics.page,
+				path: '/hh-analytics',
+				title: Header.NAV_HH_ANALYTICS,
+				icon: 'skills',
+			},
+			{ title: Header.NAV_ATS, link: '#', path: '#', icon: 'clipboardText' },
+		],
 	},
 ];
--- src/widgets/Header/ui/HeaderNav/types/headerNavTypes.ts
+++ src/widgets/Header/ui/HeaderNav/types/headerNavTypes.ts
@@ -1,5 +1,15 @@
 export interface NavItem {
+	id: string;
 	href: string;
 	path: string;
 	label: string;
+	columns?: {
+		id: string;
+		title: string;
+		subitems: {
+			name: string;
+			href: string;
+			path?: string;
+		}[];
+	}[];
 }
