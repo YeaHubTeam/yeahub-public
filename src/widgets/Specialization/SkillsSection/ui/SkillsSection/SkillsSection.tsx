86d39f19c90bcd447c5e38dc9225074f6078540f
Denis Pereloma <den.pereloma@gmail.com>
2026-06-17 16:26:33 +0300

    YH-2079: refactor

--- src/widgets/Specialization/SkillsSection/ui/SkillsSection/SkillsSection.tsx
+++ src/widgets/Specialization/SkillsSection/ui/SkillsSection/SkillsSection.tsx
@@ -3,8 +3,8 @@ import { useTranslations } from 'next-intl';
 import { Skill } from '@/entities/skill';
 import { Specializations, i18Namespace } from '@/shared/config';
 import { Chip } from '@/shared/ui/Chip';
+import { SectionWrapper } from '@/shared/ui/SectionWrapper';
 import { CollapsibleChipGrid } from '@/widgets/Specialization/CollapsibleChipGrid';
-import { SectionWrapper } from '@/widgets/Specialization/SectionWrapper';
 
 interface SkillsSectionProps {
 	skills?: Skill[];
